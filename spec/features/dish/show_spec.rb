45d7bb26cea73d0b66b6a2ae5bf42c072ffece45
Ashkan Abbasi <airashkan@yahoo.com>
2020-07-24 09:45:58 -0600

    Make Dishes controller, routes for show, and test for show

---
+++ spec/features/dish/show_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Dish show page' do
+  describe "As a visitor" do
+    it "Can see a list of ingredients and the chef's name" do
+      jim = Chef.create(name: "Jim")
+      dish1 = Dish.create(name: "Potato Salad", description: "Potato salad with mustard", chef_id: jim.id)
+      dish2 = Dish.create(name: "Green Beans", description: "Green Beans with butter", chef_id: jim.id)
+      potatoes = Ingredient.create(name: "potatoes", calories: 400)
+      potatoes = Ingredient.create(name: "mustard", calories: 50)
+      green_beans = Ingredient.create(name: "green beans", calories: 100)
+      butter = Ingredient.create(name: "butter", calories: 300)
+
+      DishIngredient.create(dish: dish1, ingredient: potatoes)
+      DishIngredient.create(dish: dish1, ingredient: butter)
+      DishIngredient.create(dish: dish2, ingredient: green_beans)
+      DishIngredient.create(dish: dish1, ingredient: butter)
+
+      visit "dishes/#{dish1.id}"
+
+      expect(page).to have_content(dish1.name)
+      expect(page).to have_content(dish1.chef.name)
+      expect(page).to have_content(potatoes.name)
+      expect(page).to have_content(butter.name)
+
+    end
+  end
+end
+
+# Story 1 of 3
+# As a visitor
+# When I visit a dish's show page
+# I see a list of ingredients for that dish
+# and the chef's name

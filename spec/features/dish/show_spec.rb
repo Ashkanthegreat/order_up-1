3aa8bd723fd949b52afbeea30012f8616c62c13b
Ashkan Abbasi <airashkan@yahoo.com>
2020-07-24 09:51:12 -0600

    Finish US1

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe 'Dish show page' do
       dish1 = Dish.create(name: "Potato Salad", description: "Potato salad with mustard", chef_id: jim.id)
       dish2 = Dish.create(name: "Green Beans", description: "Green Beans with butter", chef_id: jim.id)
       potatoes = Ingredient.create(name: "potatoes", calories: 400)
-      potatoes = Ingredient.create(name: "mustard", calories: 50)
+      mustard = Ingredient.create(name: "mustard", calories: 50)
       green_beans = Ingredient.create(name: "green beans", calories: 100)
       butter = Ingredient.create(name: "butter", calories: 300)
 
       DishIngredient.create(dish: dish1, ingredient: potatoes)
-      DishIngredient.create(dish: dish1, ingredient: butter)
+      DishIngredient.create(dish: dish1, ingredient: mustard)
       DishIngredient.create(dish: dish2, ingredient: green_beans)
       DishIngredient.create(dish: dish1, ingredient: butter)
 
@@ -20,15 +20,9 @@ RSpec.describe 'Dish show page' do
 
       expect(page).to have_content(dish1.name)
       expect(page).to have_content(dish1.chef.name)
-      expect(page).to have_content(potatoes.name)
+      expect(page).to have_content(mustard.name)
       expect(page).to have_content(butter.name)
 
     end
   end
 end
-
-# Story 1 of 3
-# As a visitor
-# When I visit a dish's show page
-# I see a list of ingredients for that dish
-# and the chef's name

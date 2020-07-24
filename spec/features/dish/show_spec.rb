09010e0993a9e99e35d79a3df3305008eed7aa94
Ashkan Abbasi <airashkan@yahoo.com>
2020-07-24 10:03:33 -0600

    Finish US2

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -14,15 +14,29 @@ RSpec.describe 'Dish show page' do
       DishIngredient.create(dish: dish1, ingredient: potatoes)
       DishIngredient.create(dish: dish1, ingredient: mustard)
       DishIngredient.create(dish: dish2, ingredient: green_beans)
-      DishIngredient.create(dish: dish1, ingredient: butter)
+      DishIngredient.create(dish: dish2, ingredient: butter)
 
       visit "dishes/#{dish1.id}"
 
       expect(page).to have_content(dish1.name)
       expect(page).to have_content(dish1.chef.name)
       expect(page).to have_content(mustard.name)
-      expect(page).to have_content(butter.name)
+      expect(page).to_not have_content(butter.name)
+    end
 
+    it "can see the total calorie count for that dish" do
+      jim = Chef.create(name: "Jim")
+      dish1 = Dish.create(name: "Potato Salad", description: "Potato salad with mustard", chef_id: jim.id)
+
+      potatoes = Ingredient.create(name: "potatoes", calories: 400)
+      mustard = Ingredient.create(name: "mustard", calories: 50)
+
+      DishIngredient.create(dish: dish1, ingredient: potatoes)
+      DishIngredient.create(dish: dish1, ingredient: mustard)
+
+      visit "dishes/#{dish1.id}"
+
+      expect(page).to have_content(450)
     end
   end
 end
